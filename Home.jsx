6c25e95e78174aff409b5c92034d9778c8e0f348
cybaguy <62558951+cybaguy@users.noreply.github.com>
2022-09-01 21:16:31 -0400

    Delete Home.jsx

--- Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function home(props){
-    return(
-        <div className="1">
-     <h1 className='home_header'>Quizzical</h1>
-      <h3 className='home_desc'>Some description if needed</h3>
-      <button className='start_quiz' onClick={props.flipPage}>Start Quiz</button>
-     </div>
-    )
-}
